df5e4329e6af40b9c93ae6ece50cc4d71c5ecc20
sachingowdaac <58177201+sachingowdaac@users.noreply.github.com>
2020-02-19 12:54:37 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
+
+    function highlightLink() {
+        const linkCoords = this.getBoundingClientRect();
+        console.log(linkCoords);
+        highlight.style.width = `${linkCoords.width}px`;
+        highlight.style.height = `${linkCoords.height}px`;
+        highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+    }
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
